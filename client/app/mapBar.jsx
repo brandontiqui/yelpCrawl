d128afdc36f6053e2fdee27054d53faf7b2237a8
Brandon Tiqui <btiqui@gmail.com>
2016-09-17 10:41:33 -0700

    Change mapBar to class component

--- client/app/mapBar.jsx
+++ client/app/mapBar.jsx
@@ -1,24 +1,102 @@
 import React from 'react';
-import {Map, Marker} from 'google-maps-react'
+import {Map, Marker, InfoWindow, Listing} from 'google-maps-react'
 
 
-var mapBar = (props) => {
+class mapBar extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {}
+		};
+	}
+
+	onMarkerClick(props, marker, e) {
+		console.log('clicked marker');
+		console.log('props: ', props);
+		console.log('marker: ', marker);
+
+		this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+	}
+
+	onMapClicked (props) {
+		if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    }
+  console.log('map clicked');
+	}
+
+
+ //  render () {
+ //  return (
+ //    <div>
+ //      <Map google={window.google} onClick={this.onMapClicked.bind(this)}>
+	// {this.props.waypoints.map(bar => {
+	//    return (
+	//      <Marker
+	// 	 position={{
+	// 	   lat: bar.location.coordinate.latitude,
+	// 	   lng: bar.location.coordinate.longitude
+	// 	 }}
+	// 	 onClick={ this.onMarkerClick.bind(this) } 
+	// 	 name={'Current location'} >
+
+	// 	 	<InfoWindow
+	// 	 	  onClose={this.onInfoWindowClose}
+	// 	 	  marker={this.state.activeMarker}
+ //        visible={this.state.showingInfoWindow}>
+ //            <div>
+ //              <h1>test</h1>
+ //            </div>
+	// 	 	</InfoWindow>
+		 
+	// 	  </Marker>
+	//    );
+	//  })}
+
+ //      </Map>
+	    
+ //    </div>
+ //  );
+ //  }
+
+  render () {
   return (
-    <div>
-      <Map google={window.google}>
-	{props.waypoints.map(bar => {
+
+	{this.props.waypoints.map(bar => {
 	   return (
+      <Map google={window.google} onClick={this.onMapClicked.bind(this)}>
 	     <Marker
-		 name={bar.name}
 		 position={{
 		   lat: bar.location.coordinate.latitude,
 		   lng: bar.location.coordinate.longitude
-		 }} />
+		 }}
+		 onClick={ this.onMarkerClick.bind(this) } 
+		 name={'Current location'} />
+		 	<InfoWindow
+		 	  onClose={this.onInfoWindowClose}
+		 	  marker={this.state.activeMarker}
+        visible={this.state.showingInfoWindow}>
+            <div>
+              <h5>{ bar.name }</h5>
+              <p>some info..</p>
+            </div>
+		 	</InfoWindow>
+      </Map>
 	   );
 	 })}
-      </Map>
-    </div>
+
+	    
   );
+  }
 };
 
 
